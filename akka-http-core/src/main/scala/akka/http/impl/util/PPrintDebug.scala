e332504a5f0ccf34d7d047a427b08f0bd036b21b
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-08-12 17:03:56 +0200

    pprint-goodies

---
+++ akka-http-core/src/main/scala/akka/http/impl/util/PPrintDebug.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+package akka.http.impl.util
+
+import java.nio.charset.Charset
+
+import akka.stream.scaladsl.{ Keep, Flow, BidiFlow }
+import akka.stream.stage.{ TerminationDirective, SyncDirective, Context, PushPullStage }
+import akka.util.ByteString
+
+import pprint._
+import pprint.Config.Defaults._
+import pprint.Colors
+
+object PPrintDebug {
+  implicit def bytestringPrinter: PPrinter[ByteString] =
+    PPrinter[ByteString] { (bs: ByteString, c: Config) ⇒
+      val maxBytes = 16 * 5
+      val utf8 = Charset.forName("utf8")
+      def formatBytes(bs: ByteString): String = {
+        def asHex(b: Byte): String = b formatted "%02X"
+        def asASCII(b: Byte): Char =
+          if (b >= 0x20 && b < 0x7f) b.toChar
+          else '.'
+
+        def formatLine(bs: ByteString): String = {
+          val data = bs.toSeq
+          val indent = " " * (c.indent + 1)
+          val hex = data.map(asHex).mkString(" ")
+          val ascii = data.map(asASCII).mkString
+          f"$indent%s$hex%-48s | $ascii"
+        }
+
+        val first = bs.take(maxBytes).grouped(16)
+        s"ByteString(${bs.size} bytes) first $maxBytes bytes:\n" +
+          first.map(formatLine).mkString("\n")
+      }
+
+      Iterator(formatBytes(bs))
+    }
+
+  def layer[L: PPrint, R: PPrint](tag: String): BidiFlow[L, L, R, R, Unit] =
+    BidiFlow.wrap(Flow[L].via(flow(tag + "-up")), Flow[R].via(flow(tag + "-down")))(Keep.none)
+
+  private[http] def flow[T: PPrint](marker: String): Flow[T, T, Unit] =
+    Flow[T].transform(() ⇒ new PushPullStage[T, T] {
+      override def onPush(element: T, ctx: Context[T]): SyncDirective = {
+        println(s"$marker: ${formatElement(element)}")
+        ctx.push(element)
+      }
+      override def onPull(ctx: Context[T]): SyncDirective = {
+        println(s"$marker: PULL")
+        ctx.pull()
+      }
+      override def onUpstreamFailure(cause: Throwable, ctx: Context[T]): TerminationDirective = {
+        println(s"$marker: Error $cause")
+        super.onUpstreamFailure(cause, ctx)
+      }
+      override def onUpstreamFinish(ctx: Context[T]): TerminationDirective = {
+        println(s"$marker: Complete")
+        super.onUpstreamFinish(ctx)
+      }
+      override def onDownstreamFinish(ctx: Context[T]): TerminationDirective = {
+        println(s"$marker: Cancel")
+        super.onDownstreamFinish(ctx)
+      }
+
+      def formatElement(element: T): String =
+        pprint.tokenize(element, width = 80, height = 10, colors = Colors.Colored).mkString
+    })
+}
